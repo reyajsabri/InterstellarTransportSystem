452b4b3f8adbc24683d84ce685734c7a06c16b7f
reyaj.ahmed <reyaj.ahmed@IRIS-CSG-1623.irissoftware.com>
2019-05-28 07:55:57 +0530

    Commit

--- InterstellarTransportSystem/src/main/java/com/interstellar/transport/app/Application.java
+++ InterstellarTransportSystem/src/main/java/com/interstellar/transport/app/Application.java
@@ -2,7 +2,6 @@ package com.interstellar.transport.app;
 
 import java.io.File;
 
-import org.hsqldb.server.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -11,13 +10,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.interstellar.transport.app.entity.DBServerUtil;
+
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableConfigurationProperties
 @EntityScan(basePackages = {"com.interstellar.transport.app"})
 public class Application extends SpringBootServletInitializer {
 
-	private static final Server sv = new Server();
 	 @Override
 	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	        return application.sources(Application.class);
@@ -26,13 +26,12 @@ public class Application extends SpringBootServletInitializer {
 	@SuppressWarnings("static-access")
 	public static void main (String[] args) {
 		
-		String[] arr = {};
-		sv.main(arr);
+		DBServerUtil.startHSQLDB();
 		
 		Runtime r=Runtime.getRuntime();  
 		r.addShutdownHook(new Thread() {
 			public void run() {
-				sv.shutdown();
+				DBServerUtil.stopHSQLDB();
 				for(String fileName : new File("test.*").list()) {
 					File file = new File(fileName);
 					if(file.exists())
@@ -43,4 +42,6 @@ public class Application extends SpringBootServletInitializer {
 		});
 		SpringApplication.run(Application.class, args);
     }
+	
+	
 }
